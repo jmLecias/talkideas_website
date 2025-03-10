4afefb517e6563c6066b58734f84948afcdbe520
jmLecias <johnmarklecias.lsd@gmail.com>
2025-03-10 23:01:34 +0800

    Updated NavBar component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -10,7 +10,7 @@
                     <div class="hero-content wow fadeInUp mx-auto max-w-[780px] text-center" data-wow-delay=".2s">
                         <h1
                             class="mb-6 text-3xl font-bold leading-snug text-white sm:text-4xl sm:leading-snug lg:text-5xl lg:leading-[1.2]">
-                            Open-Source Web Template for SaaS, Startup, Apps, and More
+                            Education and Training Center for German Language, English, Math, and More
                         </h1>
                         <p
                             class="mx-auto mb-9 max-w-[600px] text-base font-medium text-white sm:text-lg sm:leading-[1.44]">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,18 @@
 import Landingpage from '@/components/Landingpage.vue';
 import NavBar from '@/components/NavBar.vue';
 import Hero from '@/components/Hero.vue';
+
+const navLinks = [
+  { text: 'Home', href: '/' },
+  { text: 'Payment', href: '/payment' },
+  { text: 'About', href: '/about' },
+  { text: 'Blog', href: '/blog' },
+];
 </script>
 
 <template>
   <main class="home-view">
-    <NavBar />
+    <NavBar :navLinkItems="navLinks" />
     <Hero />
     <!-- <Landingpage /> -->
   </main>
